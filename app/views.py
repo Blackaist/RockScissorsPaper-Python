64b722f2dd9399de1caa84eed7b517391386587a
Alexandr <sasha97.21@mail.ru>
2017-08-22 22:04:54 +0300

    Gameable
    Signed-off-by: Alexandr <sasha97.21@mail.ru>

--- app/views.py
+++ app/views.py
@@ -1,8 +1,21 @@
+import random
+
 from django.shortcuts import render
 
+imgs = ['paper.jpg', 'scissors.jpg', 'rock.jpg']
+
 context = {
-    'human_choise': 'scissors.jpg'
+    'human_choice': 'scissors.jpg',
+    'AI_choice': 'scissors.jpg',
+    'result_text': '',
+    'score_text': 'Счет: ',
+    'wins': 0,
+    'loses': 0
 }
+win_text = "Вы победили!"
+lose_text = "Вы проиграли!"
+draw_text = "Ничья!"
+
 
 def post_list(request):
     if 'btnPaper.x' in request.POST or 'btnScissors.x' in request.POST or 'btnRock.x' in request.POST:
@@ -10,15 +23,40 @@ def post_list(request):
     return render(request, 'app/post_list.html', context=context)
 
 
-def imgChoise(request, str):
-    context['human_choise'] = str
+def imgChangeHumanChoise(request, str):
+    context['human_choice'] = str
+
+
+def imgChangeAIChoise(request, str):
+    context['AI_choice'] = str
+
+
+def compareTwoChooses():
+    if context['human_choice'] == context['AI_choice']:
+        context['result_text'] = draw_text
+    else:
+        if context['human_choice'] == imgs[0] and context['AI_choice'] == imgs[2]\
+                or context['human_choice'] == imgs[1] and context['AI_choice'] == imgs[0]\
+                or context['human_choice'] == imgs[2] and context['AI_choice'] == imgs[1]:
+            context['wins'] += 1
+            context['result_text'] = win_text
+        else:
+            context['loses'] += 1
+            context['result_text'] = lose_text
+
+
+def updateScore():
+    context['score_text'] = 'Счет: ' + str(context['wins']) + ' - ' + str(context['loses'])
 
 
 def button_click(request):
     if 'btnPaper.x' in request.POST:
-        imgChoise(request, "paper.jpg")
+        imgChangeHumanChoise(request, imgs[0])
     elif 'btnScissors.x' in request.POST:
-        imgChoise(request, "scissors.jpg")
+        imgChangeHumanChoise(request, imgs[1])
     else:
-        imgChoise(request, "rock.jpg")
-    print(context['human_choise'])
+        imgChangeHumanChoise(request, imgs[2])
+
+    imgChangeAIChoise(request, random.choice(imgs))
+    compareTwoChooses()
+    updateScore()
