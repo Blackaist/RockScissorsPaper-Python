1ffc4db1aee6c042197e3b82cbc2884c61bafb84
Alexandr <sasha97.21@mail.ru>
2017-10-10 01:05:36 +0300

    Fixes and optimization
    Signed-off-by: Alexandr <sasha97.21@mail.ru>

--- WebTest/settings.py
+++ WebTest/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '-kpoa&74nvcd$o)38h0_89$&@21+*eve9-serx79+)ey(06faw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
