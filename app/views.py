5247d3a063ea0985ec8f24ec1e6e8dce5c4a6b1a
Irina <telis5@yandex.ru>
2017-10-29 14:41:58 +0300

    Update views.py

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 from django.http import JsonResponse
 from django.shortcuts import render
 
+
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.contrib.auth import get_user_model
